732409f1104f5f6c3200a5d54b538d77ba9c9c03
LucasAlek <lucas.alek@icarotech.com>
2021-10-04 08:22:00 -0300

    organising components into folders

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import Expenses from "./components/Expenses"
+import Expenses from "./components/Expenses/Expenses";
 
 function App() {
   const expenses = [
@@ -21,13 +21,13 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ]
+  ];
   return (
     <div>
       <h2>Let's get started!</h2>
       <Expenses expenses={expenses} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
