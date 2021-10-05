9c1206f5c4723d3d78a9353eac3186db20acd219
LucasAlek <lucas.alek@icarotech.com>
2021-10-05 08:11:09 -0300

    ExpenseItem modification

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,20 +1,26 @@
-import "./ExpenseItem.css";
-import ExpenseDate from "./ExpenseDate";
-import Card from "../UI/Card";
+import React, { useState } from "react"
+
+import "./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate"
+import Card from "../UI/Card"
 
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString("pt-BR", { month: "long" });
-  const day = props.date.toLocaleString("pt-BR", { day: "2-digit" });
-  const year = props.date.getFullYear();
+  const [title, setTitle] = useState(props.title)
+
+  const clickHandler = () => {
+    setTitle("Atualizado!!")
+    console.log(title)
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">R$ {props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
-  );
+  )
 }
 
-export default ExpenseItem;
+export default ExpenseItem
