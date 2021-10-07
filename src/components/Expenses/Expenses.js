6a9b0c2a8ef93f24ceccf2a3537be7b00cba72cf
alekgomes <lucas.maistro@gmail.com>
2021-10-07 20:02:30 -0300

    add filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,28 +1,36 @@
 import "./Expenses.css"
 import ExpenseItem from "./ExpenseItem"
-
+import ExpensesFilter from "./ExpensesFilter"
+import { useState } from "react"
 
 function Expenses(props) {
+  const [enteredDate, setEnteredDate] = useState(new Date())
+
   const expenses = props.expenses
+
+  const dateChangeHandler = (e) => setEnteredDate(new Date(e.target.value))
   return (
-    <div className="expenses">
-      <ExpenseItem
-        title={expenses[0].title}
-        date={expenses[0].date}
-        amount={expenses[0].amount}
-      />
+    <div>
+      <ExpensesFilter onDateChange={dateChangeHandler} />
+      <div className="expenses">
+        <ExpenseItem
+          title={expenses[0].title}
+          date={enteredDate}
+          amount={expenses[0].amount}
+        />
 
-      <ExpenseItem
-        title={expenses[1].title}
-        date={expenses[1].date}
-        amount={expenses[1].amount}
-      />
+        <ExpenseItem
+          title={expenses[1].title}
+          date={expenses[1].date}
+          amount={expenses[1].amount}
+        />
 
-      <ExpenseItem
-        title={expenses[2].title}
-        date={expenses[2].date}
-        amount={expenses[2].amount}
-      />
+        <ExpenseItem
+          title={expenses[2].title}
+          date={expenses[2].date}
+          amount={expenses[2].amount}
+        />
+      </div>
     </div>
   )
 }
